0392714a11da440909c887b945c786a995c6634e
Anton Rudacov <anton.rudacov@gmail.com>
2018-06-03 14:49:12 +0200

    Mvn quickstart + npm init

---
+++ src/main/java/de/tuberlin/tubit/gitlab/aalss18/App.java
@@ -0,0 +1,13 @@
+package de.tuberlin.tubit.gitlab.aalss18;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
