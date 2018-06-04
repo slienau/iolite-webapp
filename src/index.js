f1f425937e7fe0061e01fb436fad5aaf9b3fec77
Anton Rudacov <anton.rudacov@gmail.com>
2018-06-04 20:17:51 +0200

    Added Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap';
+import './scss/App.scss';
+
 import App from "./App.js";
+
 ReactDOM.render(
     <App />,
     document.getElementById("root")
--- webpack.config.js
+++ webpack.config.js
@@ -10,24 +10,54 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel-loader',
-                options: { presets: ['env', 'react'] }
+                options: {presets: ['env', 'react']}
             },
             {
                 test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
     },
-    resolve: { extensions: ['*', '.js', '.jsx'] },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
     output: {
         publicPath: bundlePath,
         filename: "bundle.js"
     },
     devServer: {
-        contentBase: path.join(__dirname,'public'),
+        contentBase: path.join(__dirname, 'public'),
         port: 3000,
         publicPath: "http://localhost:3000/dist",
         open: true
     },
-    plugins: [ new webpack.HotModuleReplacementPlugin() ]
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 };
