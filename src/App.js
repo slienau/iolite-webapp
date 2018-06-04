a7aa367073440ad71aa156ad3bd224c273cdcbbc
Sebastian Lienau <mail@sebastian-lienau.de>
2018-06-04 23:36:02 +0200

    Add more example data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import "./App.css"
 import {Line} from 'react-chartjs-2';
+import {Pie} from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
 
 class App extends Component {
     render() {
@@ -8,10 +10,11 @@ class App extends Component {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
               {
-                label: 'My First dataset',
+                label: 'Boiler',
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                data: [65, 59, 80, 81, 56, 55, 40],
                 fill: false,
                 lineTension: 0.1,
-                backgroundColor: 'rgba(75,192,192,0.4)',
                 borderColor: 'rgba(75,192,192,1)',
                 borderCapStyle: 'butt',
                 borderDash: [],
@@ -25,15 +28,40 @@ class App extends Component {
                 pointHoverBorderColor: 'rgba(220,220,220,1)',
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
-                pointHitRadius: 10,
-                data: [65, 59, 80, 81, 56, 55, 40]
+                pointHitRadius: 10
+              },
+              {
+                label: 'Fridge',
+                backgroundColor: 'rgba(255,153,0,0.4)',
+                data: [60, 35, 65, 59, 95, 89, 20],
+                fill: false,
+                lineTension: 0.1,
+                borderColor: 'rgba(255,153,0,0.4)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(255,153,0,0.4)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(255,153,0,0.4)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10
               }
             ]
           };
         return (
             <div className="App">
                 <h1>Hello from React with style!</h1>
-                <Line data={data} />
+                <h2>Example Line Chart</h2>
+                <Line
+                    data={data}
+                    width={500}
+                    height={300}
+                />
             </div>
         );
     }
