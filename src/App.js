e5a57858d4aba0825872461f82c654e6c1d86e12
Sebastian Lienau <mail@sebastian-lienau.de>
2018-06-04 23:51:23 +0200

    Add more charts

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Bar} from 'react-chartjs-2';
 
 class App extends Component {
     render() {
-        const data = {
+        const data_line = {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
               {
@@ -53,15 +53,63 @@ class App extends Component {
               }
             ]
           };
+          const data_pie = {
+            labels: [
+                'Red',
+                'Green',
+                'Yellow'
+            ],
+            datasets: [{
+                data: [300, 50, 100],
+                backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+                ],
+                hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+                ]
+            }]
+        };
+        const data_bar = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+              {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: [65, 59, 80, 81, 56, 55, 40]
+              }
+            ]
+          };
         return (
             <div className="App">
                 <h1>Hello from React with style!</h1>
+
                 <h2>Example Line Chart</h2>
                 <Line
-                    data={data}
+                    data={data_line}
                     width={500}
                     height={300}
                 />
+
+                <h2>Pie Example</h2>
+                <Pie data={data_pie} />
+
+                <Bar
+                        data={data_bar}
+                        width={100}
+                        height={50}
+                        options={{
+                            maintainAspectRatio: false
+                        }}
+                />
+
             </div>
         );
     }
