6f7db668b77d614f31115f385f8b0cdcb9760ac0
Anton Rudacov <anton.rudacov@gmail.com>
2018-06-05 00:11:28 +0200

    New setup

--- src/main/java/de/tuberlin/tubit/gitlab/aalss18/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package de.tuberlin.tubit.gitlab.aalss18;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,63 @@
-var path = require('path');
+const path = require("path");
+const webpack = require("webpack");
+const bundlePath = path.resolve(__dirname, "dist/");
 
 module.exports = {
-    entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    debug: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
-    },
+    entry: "./src/index.js",
     module: {
-        loaders: [
+        rules: [
             {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {presets: ['env', 'react']}
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
-    }
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        publicPath: bundlePath,
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist",
+        open: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 };
