f507a5ed8082b0f319d6f2d091a0e9271b58406e
Sebastian Lienau <mail@sebastian-lienau.de>
2018-06-05 23:23:32 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { Component } from "react";
-import "./css/App.css"
+import React, { Component } from 'react';
+import './css/App.css'
 import './css/custom.css'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import { Navbar } from './components/navbar'
 import { Footer } from './components/footer'
+import { Dashboard } from './components/dashboard'
+import { Settings } from './components/settings'
 import { ExampleCharts } from './components/examplecharts'
+
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <main role="main" className="container">
-                    <ExampleCharts/>
+
+                <main id="main-content" role="main" className="container">
+                    <Router>
+                        <div>
+                            <Route exact path="/" component={Dashboard} />
+                            <Route path="/examplecharts" component={ExampleCharts} />
+                            <Route path="/settings" component={Settings} />
+                        </div>
+                    </Router>
                 </main>
                 <Footer />
             </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,29 +1,32 @@
 import React from "react";
+import { HashRouter as Router, Link } from 'react-router-dom'
 
 export class Navbar extends React.Component {
     render() {
         return (
-            <header>
-                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                    <a className="navbar-brand" href="#">Fixed navbar</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarCollapse">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </header>
+            <Router>
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                        <Link to="/" className="navbar-brand">Energy UI</Link>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarCollapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <Link to="/" className="nav-link">Dashboard <span className="sr-only">(current)</span></Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/examplecharts" className="nav-link">Example Charts</Link>
+                                </li>
+                                <li className="nav-item">
+                                <Link to="/settings" className="nav-link">Settings</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </header>
+            </Router>
         );
     }
 }
