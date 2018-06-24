4f02b9604eb7fff3048d448f0c461da5039ee2fc
Daniel <daniel.yermakov@campus.tu-berlin.de>
2018-06-24 21:47:24 +0200

    Table view with header reading from JSON

--- src/containers/Tables/tables.js
+++ src/containers/Tables/tables.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
 
+
+var columnMetaData = [
+    {
+        name : "id",
+        title : "Device ID"
+    },
+    {
+        name : "timestamp",
+        title : "Time"
+    },
+    {
+        name : "cons",
+        title : "Consumption"
+    }
+]
+
+var TableData = [
+    {
+      id : "1",
+      timestamp : "1-06-2018",
+      cons : "35.6"
+    },
+    {
+        id : "2",
+        timestamp : "2-06-2018",
+        cons : "37.0"
+    },
+    {
+        id : "1",
+        timestamp : "3-06-2018",
+        cons : "45.2"
+    }
+
+]
+
+
+const TableHead = (prop) => {
+    //console.log(prop.list);
+    return(
+
+            <thead>
+              <tr>
+                {prop.list.map(item => (
+                    <th>{item.title}</th>
+
+            ))}
+              </tr>
+            </thead>
+
+)
+}
+
+
 class Tables extends Component {
+
+
+
+
 
   render() {
     return (
-
-        <div>
-          <h1>Tables</h1>
-        </div>
+        <div class="table-responsive">
+          <table class="table table-striped table-sm">
+            <TableHead list={columnMetaData}> </TableHead>
+          </table>
+          </div>
     );
   }
 }
+
+
+
 
 export default Tables;
