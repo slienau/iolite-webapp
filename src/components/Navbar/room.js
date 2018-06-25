df3819964105943e4b23d8d770d317575d93f886
Sebastian Lienau <mail@sebastian-lienau.de>
2018-06-26 00:01:00 +0200

    Better navbar

--- src/components/Navbar/room.js
+++ src/components/Navbar/room.js
@@ -6,21 +6,33 @@ export class Room extends React.Component {
         return (
             <li>
                 <a className="btn btn-primary room-room" data-toggle="collapse" href={"#" + collapseId} role="button" aria-expanded="false" aria-controls={collapseId}>{this.props.name}</a>
-                <ul className="collapse show" id={collapseId}>
-                    {this.props.devices.map(device => <li className="room-device"><Switch />{device}</li>)}
-                </ul>
+                <div className="collapse show" id={collapseId}>
+                    <table className="switchtable">
+                        {
+                            this.props.devices.map(device =>
+                                <tr>
+                                    <td className="room-device">
+                                            <Switch device={device} />
+                                    </td>
+                                    <td>
+                                        {device}
+                                    </td>
+                                </tr>
+                            )}
+                    </table>
+                </div>
             </li>
         );
     }
 }
 
 class Switch extends Component {
-  render() {
-    return (
-      <label class="switch">
-        <input type="checkbox"></input>
-        <span class="slider round"></span>
-      </label>
-    )
-  }
+    render() {
+        return (
+            <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span>
+            </label>
+        )
+    }
 }
