010c63002b7e91bfdfed1c6bb1b012ad71b3bce7
tom <gill@campus.tu-berlin.de>
2018-06-25 21:03:38 +0200

    settings page working again with new structure

--- src/containers/Settings/settings.js
+++ src/containers/Settings/settings.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 
+import {
+    Toolbar, Group, SettingsPanel, Switch, Checkbox, CheckboxGroup,
+    Radio, RadioGroup, Option, Selection, Input, TextArea
+} from 'react-settings-panel'
+
+
+
 class Settings extends Component {
 
-  render() {
-    return (
+    render() {
+        return (
 
-        <div>
-          <h1>Settings</h1>
-        </div>
-    );
-  }
+            <SettingsPanel color={'#728ad8'} onSubmit={this.handleSubmit}>
+                <Toolbar />
+                <Group>
+                    <Switch name='mySwitch' title='Switch One'/>
+                    <Input name='myTextField' title='InputField'/>
+                    <TextArea name='myTextArea' title='Text Area One'/>
+                    <CheckboxGroup name='myCheckbox' title='Checkbox One'>
+                        <Checkbox value='A' />
+                        <Checkbox value='B' />
+                        <Checkbox value='C' />
+                    </CheckboxGroup>
+                </Group>
+                <Group>
+                    <Selection title="Selection" name='mySelection' >
+                        <Option value='a' />
+                        <Option value='b' />
+                        <Option value='c' />
+                    </Selection>
+                    <RadioGroup title="Radio Group" name='myRadio'>
+                        <Radio value='A' />
+                        <Radio value='B' />
+                        <Radio value='C' />
+                    </RadioGroup>
+                </Group>
+
+            </SettingsPanel>
+        );
+    }
 }
 
 export default Settings;
