90b28f5ca90902fca706ed9b8e67d3545d607d14
Sebastian Lienau <mail@sebastian-lienau.de>
2018-07-15 19:15:39 +0200

    Replace router by state; Change charts dataflow

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,76 @@
 import React, { Component } from 'react';
-import Router from '../Router/router';
 import Navbar from '../Navbar/navbar';
+import Charts from '../../containers/Charts/charts'
+import Tables from '../../containers/Tables/tables'
+import Settings from '../../containers/Settings/settings'
 import '../../css/Main.css'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contentPage: '',
+      chartData: {}
+    }
+    this.switchContentPage = this.switchContentPage.bind(this);
+  }
 
+  componentWillMount() {
+    this.getChartData();
+    this.switchContentPage('charts')
+  }
+
+  switchContentPage(page) {
+    this.setState({
+      contentPage: page
+    });
+  }
+
+  getChartData() {
+    // Ajax calls here
+    this.setState({
+      chartData: {
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets: [
+          {
+            label: 'Population',
+            data: [
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar switchPage={this.switchContentPage} />
         <div className="container-fluid">
           <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-            <Router />
+            {(() => {
+              switch (this.state.contentPage) {
+                case "charts": return (<Charts chartData={this.state.chartData} />);
+                case "tables": return (<Tables />);
+                case "settings": return (<Settings />);
+                default: return "Content";
+              }
+            })()}
           </main>
         </div>
       </div>
--- src/containers/Charts/charts.js
+++ src/containers/Charts/charts.js
@@ -1,45 +1,49 @@
-import React, {Component} from 'react';
-import LineChart from '../../components/Samplecharts/line';
+import React, { Component } from 'react';
+import { Bar, Line, Pie } from 'react-chartjs-2';
 
-class Charts extends Component {
-
-  state = {
-
-    powerUsage: []
+export default class Charts extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
+    }
   }
-
-  componentDidMount() {
-
-    fetch('./resources/power.json')
-        .then(data => data.json())
-        .then(json => this.setState({powerUsage: json}));
-
-    console.log("Charts container mounted!");
+  static defaultProps = {
+    displayTitle:false,
+    displayLegend: true,
+    legendPosition:'right',
+    location:'City'
   }
 
   render() {
-
-    const {powerUsage} = this.state;
-    console.log(powerUsage);
-
     return (
-
-        <div>
-          <div
-              className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 className="h2">Energy consumption</h1>
-            <div className="btn-toolbar mb-2 mb-md-0">
-              <button className="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
+      <div>
+        <div
+          className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 className="h2">Energy consumption</h1>
+          <div className="btn-toolbar mb-2 mb-md-0">
+            <button className="btn btn-sm btn-outline-secondary dropdown-toggle">
+              <span data-feather="calendar"></span>
+              This week
               </button>
-            </div>
           </div>
-
-          <LineChart/>
         </div>
+
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              fontSize: 25
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
+        />
+
+      </div>
     );
   }
 }
-
-export default Charts;
