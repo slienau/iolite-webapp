d5f4ebc84cce9aa48cb64bb2ee21ee4ba8d3ad58
Sebastian Lienau <mail@sebastian-lienau.de>
2018-07-16 12:31:05 +0200

    Different colors per device

--- src/containers/Charts/charts.js
+++ src/containers/Charts/charts.js
@@ -5,14 +5,14 @@ export default class Charts extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chartData: {}
+      chartData: {},
+      color_pool: ["#001f3f","#FF851B","#0074D9","#FF4136","#7FDBFF","#85144b","#39CCCC", "#F012BE", "#3D9970", "#B10DC9", "#2ECC40", "#AAAAAA", "#FFDC00", "#DDDDDD", "AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
     }
   }
   static defaultProps = {
     displayTitle: false,
     displayLegend: true,
-    legendPosition: 'right',
-    location: 'City'
+    legendPosition: 'top',
   }
 
   componentWillMount() {
@@ -22,19 +22,21 @@ export default class Charts extends Component {
     });
   }
 
-  getChartData(){
+  getChartData() {
     const restData = this.props.restData;
     const rooms = this.props.restData.rooms;
-    
+
     var datasets = [];
 
     rooms.forEach(room => {
       room.devices.forEach(device => {
+        var device_color = this.state.color_pool.shift();
         var dataset = {
           label: device.name,
-          backgroundColor: 'red',
-          borderColor: 'red',
+          backgroundColor: device_color,
+          borderColor: device_color,
           fill: false,
+          // showLine: false,
           data: []
         };
         device.usage.forEach(usage => {
@@ -95,6 +97,11 @@ export default class Charts extends Component {
             beginAtZero: true
           }
         }]
+      },
+      elements: {
+        line: {
+          tension: 0, // disables bezier curves
+        }
       }
     }
     return (
