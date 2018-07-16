f1f1211c61b4765543adcc26aa339568e84944bc
Sebastian Lienau <mail@sebastian-lienau.de>
2018-07-16 15:21:26 +0200

    Zoom reset (not working yet)

--- src/containers/Charts/charts.js
+++ src/containers/Charts/charts.js
@@ -7,14 +7,74 @@ export default class Charts extends Component {
     super(props);
     this.state = {
       chartData: {},
-      color_pool: ["#001f3f","#FF851B","#0074D9","#FF4136","#7FDBFF","#85144b","#39CCCC", "#F012BE", "#3D9970", "#B10DC9", "#2ECC40", "#AAAAAA", "#FFDC00", "#DDDDDD", "AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+      color_pool: ["#001f3f", "#FF851B", "#0074D9", "#FF4136", "#7FDBFF", "#85144b", "#39CCCC", "#F012BE", "#3D9970", "#B10DC9", "#2ECC40", "#AAAAAA", "#FFDC00", "#DDDDDD", "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"],
+      chartOptions: {
+        responsive: true,
+        title: {
+          display: false,
+          fontSize: 25
+        },
+        legend: {
+          display: true,
+          position: 'top'
+        },
+        zoom: {
+          enabled: true,
+          drag: true,
+          mode: 'x',
+          limits: {
+            max: 10,
+            min: 0.5
+          }
+        },
+        scales: {
+          xAxes: [{
+            type: 'time',
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Date'
+            },
+            ticks: {
+              major: {
+                fontStyle: 'bold',
+                fontColor: 'grey'
+              }
+            },
+            time: {
+              displayFormats: {
+                quarter: 'MMM YYYY'
+              },
+              min: null,
+              max: null
+            }
+          }],
+          yAxes: [{
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Power Consumption'
+            },
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        },
+        elements: {
+          line: {
+            tension: 0, // disables bezier curves
+          }
+        },
+        animation: {
+          duration: 0, // general animation time
+        },
+        hover: {
+          animationDuration: 0, // duration of animations when hovering an item
+        },
+        responsiveAnimationDuration: 0, // animation duration after a resize
+      }
     }
     this.resetZoom = this.resetZoom.bind(this);
-  }
-  static defaultProps = {
-    displayTitle: false,
-    displayLegend: true,
-    legendPosition: 'top',
   }
 
   componentWillMount() {
@@ -60,68 +120,21 @@ export default class Charts extends Component {
     return chartData;
   }
 
-  resetZoom(){
+  resetZoom() {
     console.log('resetting zoom');
     // TODO: reset zoom
+    alert('TODO')
+    var newChartOptions = this.state.chartOptions;
+    newChartOptions.scales.xAxes[0].time.min = null;
+    newChartOptions.scales.xAxes[0].time.max = null;
+    
+    this.setState(prevState => ({
+      chartOptions: newChartOptions
+    }));
+    this.forceUpdate();
   }
 
   render() {
-    var chartOptions = {
-      responsive: true,
-      title: {
-        display: this.props.displayTitle,
-        fontSize: 25
-      },
-      legend: {
-        display: this.props.displayLegend,
-        position: this.props.legendPosition
-      },
-      zoom: {
-        enabled: true,
-        drag: true,
-        mode: 'x',
-        limits: {
-          max: 10,
-          min: 0.5
-        }
-      },
-      scales: {
-        xAxes: [{
-          type: 'time',
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Date'
-          },
-          ticks: {
-            major: {
-              fontStyle: 'bold',
-              fontColor: 'grey'
-            }
-          },
-          time: {
-            displayFormats: {
-              quarter: 'MMM YYYY'
-            }
-          }
-        }],
-        yAxes: [{
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Power Consumption'
-          },
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      },
-      elements: {
-        line: {
-          tension: 0, // disables bezier curves
-        }
-      }
-    }
     return (
       <div>
         <div
@@ -132,13 +145,13 @@ export default class Charts extends Component {
               <span data-feather="calendar"></span>
               This week
               </button>
-              <button className="btn btn-sm btn-outline-secondary" onClick={this.resetZoom}>Reset Zoom</button>
+            <button className="btn btn-sm btn-outline-secondary" onClick={this.resetZoom}>Reset Zoom</button>
           </div>
         </div>
 
         <Line
           data={this.state.chartData}
-          options={chartOptions}
+          options={this.state.chartOptions}
         />
 
       </div>
