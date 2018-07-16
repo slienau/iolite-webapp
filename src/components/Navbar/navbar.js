9691e6de2ddc9957dcde775fa5b794b720d75f1b
Sebastian Lienau <mail@sebastian-lienau.de>
2018-07-16 19:56:10 +0200

    Render rooms from JSON in sidebar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Room } from './room'
 import MdSettings from 'react-icons/lib/md/settings'
 import MdMenu from 'react-icons/lib/md/menu'
 import MdClose from 'react-icons/lib/md/close'
@@ -37,13 +36,24 @@ export default class Navbar extends React.Component {
             <LinkButton contentPage="charts" activePage={this.props.contentPage} title="Charts" switchPage={this.props.switchPage} />
             <LinkButton contentPage="tables" activePage={this.props.contentPage} title="Tables" switchPage={this.props.switchPage} />
           </li>
-          <Room name="Living Room" devices={['Device 1', 'Device 2', 'Device 3', 'Device 4']} />
-          <Room name="Kitchen" devices={['Device 1', 'Device 2', 'Device 3']} />
-          <Room name="Bedroom" devices={['Device 1', 'Device 2']} />
-          <Room name="Bathroom" devices={['Device 1', 'Device 2', 'Device 3']} />
+          {
+            this.props.restData.rooms
+              .sort(function (a, b) { return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); }) // sort by room name
+              .map(room => (
+                <Room name={room.name}>
+                  {
+                    room.devices
+                      .sort(function (a, b) { return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); }) // sort by device name
+                      .map(device =>
+                        (<Device name={device.name} />)
+                      )
+                  }
+                </Room>
+              ))
+          }
         </ul>
         <div id="navbar-settings-icon" className="position-absolute bg-light">
-            <a onClick={() => switchPage('settings')}><MdSettings color="grey" size={36} /></a>
+          <a onClick={() => switchPage('settings')}><MdSettings color="grey" size={36} /></a>
         </div>
       </nav>
     );
@@ -51,11 +61,52 @@ export default class Navbar extends React.Component {
 }
 
 class LinkButton extends Component {
-  render(){
+  render() {
     const switchPage = this.props.switchPage;
-    const classes = (this.props.contentPage===this.props.activePage ? 'active ' : '') + "nav-link btn btn-primary float-left navbar-top-button";
-    return(
+    const classes = (this.props.contentPage === this.props.activePage ? 'active ' : '') + "nav-link btn btn-primary float-left navbar-top-button";
+    return (
       <button className={classes} onClick={() => switchPage(this.props.contentPage)}>{this.props.title}</button>
+    )
+  }
+}
+
+class Room extends React.Component {
+  render() {
+    var collapseId = this.props.name.replace(' ', '').toLowerCase()
+    return (
+      <li>
+        <a className="btn btn-secondary room-room" data-toggle="collapse" href={"#" + collapseId} role="button" aria-expanded="false" aria-controls={collapseId}>{this.props.name}</a>
+        <div className="collapse show" id={collapseId}>
+          <table className="switchtable">
+            <tbody>
+              {this.props.children}
+            </tbody>
+          </table>
+        </div>
+      </li>
+    );
+  }
+}
+
+class Device extends Component {
+  render() {
+    const deviceName = this.props.name;
+    return (
+      <tr key={deviceName}>
+        <td className="room-device"><Switch device={deviceName} /></td>
+        <td>{deviceName}</td>
+      </tr>
+    );
+  }
+}
+
+class Switch extends Component {
+  render() {
+    return (
+      <label className="switch">
+        <input type="checkbox"></input>
+        <span className="slider round"></span>
+      </label>
     )
   }
 }
