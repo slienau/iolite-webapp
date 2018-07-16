68ea3c34eb2e8d9042af247af45dfc67d45dcbe2
Anton Rudacov <a.rudacov@campus.tu-berlin.de>
2018-07-16 21:58:42 +0200

    New date-picker

--- src/containers/Charts/charts.js
+++ src/containers/Charts/charts.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
-import { Bar, Line, Pie } from 'react-chartjs-2';
-import * as zoom from 'chartjs-plugin-zoom';
+import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import {Line} from 'react-chartjs-2';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class Charts extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       chartData: {},
-      color_pool: ["#001f3f", "#FF851B", "#0074D9", "#FF4136", "#7FDBFF", "#85144b", "#39CCCC", "#F012BE", "#3D9970", "#B10DC9", "#2ECC40", "#AAAAAA", "#FFDC00", "#DDDDDD", "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"],
+      color_pool: [
+        "#001f3f",
+        "#FF851B",
+        "#0074D9",
+        "#FF4136",
+        "#7FDBFF",
+        "#85144b",
+        "#39CCCC",
+        "#F012BE",
+        "#3D9970",
+        "#B10DC9",
+        "#2ECC40",
+        "#AAAAAA",
+        "#FFDC00",
+        "#DDDDDD",
+        "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"
+      ],
       chartOptions: {
         responsive: true,
         title: {
@@ -73,7 +93,8 @@ export default class Charts extends Component {
         },
         responsiveAnimationDuration: 0, // animation duration after a resize
       }
-    }
+    };
+
     this.resetZoom = this.resetZoom.bind(this);
   }
 
@@ -82,6 +103,8 @@ export default class Charts extends Component {
     this.setState({
       chartData: chartData
     });
+
+    console.log(moment());
   }
 
   getChartData() {
@@ -136,49 +159,75 @@ export default class Charts extends Component {
 
   render() {
     return (
-      <div>
-        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 className="h2">Energy consumption</h1>
+        <div>
+          <div
+              className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 className="h2">Energy consumption</h1>
+          </div>
+
+          <FormControls/>
+
+          <Line
+              data={this.state.chartData}
+              options={this.state.chartOptions}
+          />
+
+          <button className="btn btn-sm btn-outline-secondary" onClick={this.resetZoom}>Reset Zoom</button>
+
         </div>
-
-        <FormControls />
-
-        <Line
-          data={this.state.chartData}
-          options={this.state.chartOptions}
-        />
-
-        <button className="btn btn-sm btn-outline-secondary" onClick={this.resetZoom}>Reset Zoom</button>
-
-      </div>
     );
   }
 }
 
 class FormControls extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      startDate: moment(),
+      endDate: moment()
+    };
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeEnd = this.handleChangeEnd.bind(this);
+  }
+
+  handleChangeStart(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  handleChangeEnd(date) {
+    this.setState({
+      endDate: date
+    });
+  }
+
   render() {
+
     return (
-      <div className="row">
-        <div className="form-group col-md-4">
-          <label htmlFor="datefrom">Date from</label>
-          <input type="date" className="form-control" id="datefrom" />
+        <div className="row justify-content-end">
+          <div className="col-2">
+
+            <DatePicker
+                selected={this.state.startDate}
+                selectsStart
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onChange={this.handleChangeStart}
+                dateFormatCalendar={"DD MM YYYY"}
+            />
+          </div>
+          <div className="col-2">
+            <DatePicker
+                selected={this.state.endDate}
+                selectsEnd
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onChange={this.handleChangeEnd}
+            />
+          </div>
         </div>
-        <div className="form-group col-md-4">
-          <label htmlFor="dateto">Date to</label>
-          <input type="date" className="form-control" id="dateto" />
-        </div>
-        <div className="form-group col-md-4">
-          <label htmlFor="intervalSelector">Interval</label>
-          <select className="form-control form-control-sm" id="intervalSelector">
-            <option>Year</option>
-            <option>Month</option>
-            <option>Week</option>
-            <option>Day</option>
-            <option>Hour</option>
-            <option>Minute</option>
-          </select>
-        </div>
-      </div>
     );
   }
 }
