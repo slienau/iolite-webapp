35379f654474eaf5f1597d59f217ab9d44207eeb
Sebastian Lienau <mail@sebastian-lienau.de>
2018-07-19 23:52:27 +0200

    Move DateRange component to seperate file

--- src/containers/Charts/Charts.js
+++ src/containers/Charts/Charts.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
 import {Line} from 'react-chartjs-2';
 import {colorPool, chartOptions} from './options'
-
-import 'react-datepicker/dist/react-datepicker.css';
+import DateRange from './DateRange'
 
 export default class Charts extends Component {
 
@@ -69,64 +66,6 @@ export default class Charts extends Component {
                     options={this.state.chartOptions}
                 />
 
-            </div>
-        );
-    }
-}
-
-class DateRange extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            startDate: moment(),
-            endDate: moment()
-        };
-        this.handleChangeStart = this.handleChangeStart.bind(this);
-        this.handleChangeEnd = this.handleChangeEnd.bind(this);
-    }
-
-    handleChangeStart(date) {
-        this.setState({
-            startDate: date
-        }, function () {
-            console.log(this.state.startDate);
-        });
-    }
-
-    handleChangeEnd(date) {
-        this.setState({
-            endDate: date
-        }, function () {
-            console.log(this.state.endDate);
-        });
-    }
-
-    render() {
-
-        return (
-            <div className="row justify-content-end">
-                <div className="col-2">
-                    From
-                    <DatePicker
-                        selected={this.state.startDate}
-                        selectsStart
-                        startDate={this.state.startDate}
-                        endDate={this.state.endDate}
-                        onChange={this.handleChangeStart}
-                        dateFormatCalendar={"DD MM YYYY"}
-                    />
-                </div>
-                <div className="col-2">
-                    To
-                    <DatePicker
-                        selected={this.state.endDate}
-                        selectsEnd
-                        startDate={this.state.startDate}
-                        endDate={this.state.endDate}
-                        onChange={this.handleChangeEnd}
-                    />
-                </div>
             </div>
         );
     }
