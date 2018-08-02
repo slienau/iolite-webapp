ad26dd5695830280c2d405b6a4c4ae36af529194
Sebastian Lienau <sebastian.lienau@ml-pa.com>
2018-08-02 14:14:07 +0200

    Combine Device and DeviceSwitch components for better data handling

--- src/components/Navbar/Device.js
+++ src/components/Navbar/Device.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import DeviceSwitch from './DeviceSwitch'
+import {connect} from "react-redux";
+import {toggleDeviceSwitch} from "../../redux_js/actions/homeActions";
+import './device.css'
 
 class Device extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            checked: false
+        }
+    }
+
+    handleChange() {
+        this.props.toggleDeviceSwitch(this.refs.selected.id, this.refs.selected.checked);
+        this.setState({
+            checked: this.refs.selected.checked
+        })
+    }
+
     render() {
+        let sliderStyle = {
+        }
+        if(this.state.checked) {
+            sliderStyle = {
+                backgroundColor: this.props.deviceColor
+            }
+        }
         return (
             <tr>
-                <td className="room-device"><DeviceSwitch deviceId={this.props.deviceId} deviceColor={this.props.deviceColor}/>
+                <td className="room-device">
+                    <label htmlFor={this.props.deviceId} className="switch">
+                        <input type="checkbox" id={this.props.deviceId} ref="selected"
+                               onChange={this.handleChange.bind(this)}/>
+                        <span className="slider round" style={sliderStyle}/>
+                    </label>
                 </td>
                 <td>{this.props.deviceName}</td>
             </tr>
@@ -17,7 +46,8 @@ class Device extends Component {
 Device.propTypes = {
     deviceName: PropTypes.string.isRequired,
     deviceId: PropTypes.string.isRequired,
-    deviceColor: PropTypes.string.isRequired
+    deviceColor: PropTypes.string.isRequired,
+    toggleDeviceSwitch: PropTypes.func.isRequired
 };
 
-export default Device;
+export default connect(null, {toggleDeviceSwitch: toggleDeviceSwitch})(Device);
