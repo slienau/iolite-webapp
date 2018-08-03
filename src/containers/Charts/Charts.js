af5de974b52b41a4774b9c1dc79a85e5a4033016
Sebastian Lienau <lienau@campus.tu-berlin.de>
2018-08-03 13:58:00 +0200

    Move LineChart to seperate component

--- src/containers/Charts/Charts.js
+++ src/containers/Charts/Charts.js
@@ -1,68 +1,10 @@
 import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
-import {colorPool, chartOptions} from './options'
 import DateRange from './DateRange'
 import {connect} from 'react-redux';
 import PropTypes from "prop-types";
+import LineChart from './LineChart'
 
 class Charts extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            chartData: {},
-            colorPool: colorPool,
-            chartOptions: chartOptions
-        };
-    }
-
-    componentWillMount() {
-        const chartData = this.getChartData(this.props.visibleData);
-        this.setState({
-            chartData: chartData
-        });
-    }
-
-    componentWillReceiveProps(nextProps) {
-        const chartData = this.getChartData(nextProps.visibleData);
-        this.setState({
-            chartData: chartData
-        });
-    }
-
-    getChartData(showData) {
-
-        var datasets = [];
-
-        showData.rooms.forEach(room => {
-            room.devices.forEach(device => {
-                var deviceColor = device.color;
-                var dataset = {
-                    label: device.name,
-                    backgroundColor: deviceColor,
-                    borderColor: deviceColor,
-                    borderWidth: '1',
-                    pointRadius: '0',
-                    pointHoverRadius: '3',
-                    fill: false,
-                    data: []
-                };
-                device.usage.slice(800).forEach(usage => {
-                    var dataEntry = {
-                        x: new Date(usage.timestamp),
-                        y: usage.value
-                    }
-                    dataset.data.push(dataEntry);
-                })
-                datasets.push(dataset);
-            })
-        })
-
-        const chartData = {
-            datasets: datasets
-        }
-        return chartData;
-    }
 
     render() {
         return (
@@ -70,10 +12,7 @@ class Charts extends Component {
 
                 <DateRange/>
 
-                <Line
-                    data={this.state.chartData}
-                    options={this.state.chartOptions}
-                />
+                <LineChart visibleData={this.props.visibleData}/>
 
             </div>
         );
---
+++ src/containers/Charts/LineChart.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import {Line} from 'react-chartjs-2';
+import {lineChartOptions} from './options'
+import PropTypes from "prop-types";
+
+class LineChart extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: {},
+            chartOptions: lineChartOptions
+        };
+    }
+
+    componentWillMount() {
+        const chartData = this.getChartData(this.props.visibleData);
+        this.setState({
+            chartData: chartData
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const chartData = this.getChartData(nextProps.visibleData);
+        this.setState({
+            chartData: chartData
+        });
+    }
+
+    getChartData(showData) {
+
+        var datasets = [];
+
+        showData.rooms.forEach(room => {
+            room.devices.forEach(device => {
+                var deviceColor = device.color;
+                var dataset = {
+                    label: device.name,
+                    backgroundColor: deviceColor,
+                    borderColor: deviceColor,
+                    borderWidth: '1',
+                    pointRadius: '0',
+                    pointHoverRadius: '3',
+                    fill: false,
+                    data: []
+                };
+                device.usage.slice(800).forEach(usage => { //TODO: remove 'slice(800)' when productive
+                    var dataEntry = {
+                        x: new Date(usage.timestamp),
+                        y: usage.value
+                    }
+                    dataset.data.push(dataEntry);
+                })
+                datasets.push(dataset);
+            })
+        })
+
+        const chartData = {
+            datasets: datasets
+        }
+        return chartData;
+    }
+
+    render() {
+        return (
+            <div>
+
+                <Line
+                    data={this.state.chartData}
+                    options={this.state.chartOptions}
+                />
+
+            </div>
+        );
+    }
+}
+
+LineChart.propTypes = {
+    visibleData: PropTypes.object.isRequired
+}
+
+export default LineChart;
